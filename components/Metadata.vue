3b91c8836ef3c7f5c46f5de7fb177a786d854dd7
Gianluca Monaco <mckenzie.gian@gmail.com>
2025-12-09 23:32:08 +0100

    Update debug view: style

--- components/Metadata.vue
+++ components/Metadata.vue
@@ -5,35 +5,43 @@ const meta = useSceneMeta();
 const scene = use3DScene();
 const debug = useDebug();
 const { $wsAudio } = useNuxtApp() as any;
-const difference = true;
+const difference = false;
+
+const boxClass = ['border-1 px-1', difference ? 'bg-[#eee] text-black' : 'bg-black text-[#eee]'];
 
 </script>
 
 <template>
   <div v-if="debug">
-    <div class="fixed top-0 left-0 flex flex-col gap-05 z-10 text-sm" :class="[difference && 'mix-blend-difference']">
-      <p class="px-1 bg-[#eee] text-black border-1">Controls</p>
-      <div class="flex gap-05 text-xs">
-        <p class="p-[1px] bg-[#eee] text-black text-center border-1"><span class="w-5 inline-block px-1 border-1 rounded-sm">1</span></p>
-        <p class="p-[1px] bg-[#eee] text-black text-center border-1"><span class="w-5 inline-block px-1 border-1 rounded-sm">2</span></p>
-        <p class="p-[1px] bg-[#eee] text-black text-center border-1"><span class="w-5 inline-block px-1 border-1 rounded-sm">3</span></p>
-        <p class="p-[1px] bg-[#eee] text-black text-center border-1"><span class="w-5 inline-block px-1 border-1 rounded-sm">4</span></p>
-        <p class="p-[1px] bg-[#eee] text-black text-center border-1"><span class="w-5 inline-block px-1 border-1 rounded-sm">5</span></p>
-        <p class="p-[1px] bg-[#eee] text-black text-center border-1"><span class="w-5 inline-block px-1 border-1 rounded-sm">0</span></p>
-        <p class="p-[1px] bg-[#eee] text-black text-center border-1"><span class="w-5 inline-block px-1 border-1 rounded-sm">D</span></p>
-        <p class="p-[1px] bg-[#eee] text-black text-center border-1"><span class="w-5 inline-block px-1 border-1 rounded-sm">R</span></p>
-        <p class="p-[1px] bg-[#eee] text-black text-center border-1"><span class="w-5 inline-block px-1 border-1 rounded-sm">-</span></p>
+    <div class="fixed top-0 left-0 flex flex-col gap-0 z-10 text-sm" :class="[difference && 'mix-blend-difference']">
+      <p :class="boxClass">Controls</p>
+      <div class="flex gap-0 text-xs">
+        <p class="!p-[1px] text-center" :class="boxClass"><span class="w-5 inline-block px-1 border-1 rounded-sm">1</span></p>
+        <p class="!p-[1px] text-center" :class="boxClass"><span class="w-5 inline-block px-1 border-1 rounded-sm">2</span></p>
+        <p class="!p-[1px] text-center" :class="boxClass"><span class="w-5 inline-block px-1 border-1 rounded-sm">3</span></p>
+        <p class="!p-[1px] text-center" :class="boxClass"><span class="w-5 inline-block px-1 border-1 rounded-sm">4</span></p>
+        <p class="!p-[1px] text-center" :class="boxClass"><span class="w-5 inline-block px-1 border-1 rounded-sm">5</span></p>
+        <p class="!p-[1px] text-center" :class="boxClass"><span class="w-5 inline-block px-1 border-1 rounded-sm">6</span></p>
+        <p class="!p-[1px] text-center" :class="boxClass"><span class="w-5 inline-block px-1 border-1 rounded-sm">7</span></p>
+        <p class="!p-[1px] text-center" :class="boxClass"><span class="w-5 inline-block px-1 border-1 rounded-sm">8</span></p>
+        <p class="!p-[1px] text-center" :class="boxClass"><span class="w-5 inline-block px-1 border-1 rounded-sm">9</span></p>
+        <p class="!p-[1px] text-center" :class="boxClass"><span class="w-5 inline-block px-1 border-1 rounded-sm">0</span></p>
+      </div>
+      <div class="flex gap-0 text-xs">
+        <p class="!p-[1px] text-center" :class="boxClass"><span class="w-5 inline-block px-1 border-1 rounded-sm">D</span></p>
+        <p class="!p-[1px] text-center" :class="boxClass"><span class="w-5 inline-block px-1 border-1 rounded-sm">R</span></p>
+        <p class="!p-[1px] text-center" :class="boxClass"><span class="w-5 inline-block px-1 border-1 rounded-sm">-</span></p>
       </div>
     </div>
 
-    <div class="fixed top-0 right-0 flex z-10 text-sm" :class="[difference && 'mix-blend-difference']">
+    <div class="fixed top-0 right-0 flex gap-0 z-10 text-sm" :class="[difference && 'mix-blend-difference']">
       <client-only>
-        <div v-if="$wsAudio" class="flex flex-col gap-05">
-          <p class="px-1 bg-[#eee] text-black border-1">Channels</p>
-          <div class="flex gap-05">
-            <div v-for="(channel, index) in $wsAudio" class="flex gap-05">
-              <p class="w-5 px-1 bg-[#eee] text-black border-1">{{ index }}</p>
-              <p class="w-15 px-1 bg-[#eee] text-black border-1 relative"
+        <div v-if="$wsAudio" class="flex flex-col gap-0">
+          <p :class="boxClass">Channels</p>
+          <div class="flex gap-0">
+            <div v-for="(channel, index) in $wsAudio" class="flex gap-0">
+              <p class="w-5" :class="boxClass">{{ index }}</p>
+              <p class="w-15 relative" :class="boxClass"
               >{{ channel[0]?.toFixed(2) }}
               <div
                 class="absolute top-0 left-0 h-full bg-[#000a] max-w-full"
@@ -45,29 +53,29 @@ const difference = true;
         </div>
       </client-only>
 
-      <div class="flex flex-col gap-05">
-        <p class="px-1 bg-[#eee] text-black border-1">Camera</p>
-        <div class="flex gap-05">
-          <p class="w-5 px-1 bg-[#eee] text-black text-center border-1">x</p>
-          <p class="w-15 px-1 bg-[#eee] text-black border-1">{{ scene?.camera?.position.x.toFixed(1) }}</p>
-          <p class="w-5 px-1 bg-[#eee] text-black text-center border-1">y</p>
-          <p class="w-15 px-1 bg-[#eee] text-black border-1">{{ scene?.camera?.position.y.toFixed(1) }}</p>
-          <p class="w-5 px-1 bg-[#eee] text-black text-center border-1">z</p>
-          <p class="w-15 px-1 bg-[#eee] text-black border-1">{{ scene?.camera?.position.z.toFixed(1) }}</p>
+      <div class="flex flex-col gap-0">
+        <p :class="boxClass">Camera</p>
+        <div class="flex gap-0">
+          <p class="w-5 text-center" :class="boxClass">x</p>
+          <p class="w-15" :class="boxClass">{{ scene?.camera?.position.x.toFixed(1) }}</p>
+          <p class="w-5 text-center" :class="boxClass">y</p>
+          <p class="w-15" :class="boxClass">{{ scene?.camera?.position.y.toFixed(1) }}</p>
+          <p class="w-5 text-center" :class="boxClass">z</p>
+          <p class="w-15" :class="boxClass">{{ scene?.camera?.position.z.toFixed(1) }}</p>
         </div>
       </div>
     </div>
 
     
-    <div class="fixed bottom-0 left-0 z-10 text-sm" :class="[difference && 'mix-blend-difference']">
+    <div class="fixed bottom-0 left-0 z-10 flex flex-col gap-0 text-sm" :class="[difference && 'mix-blend-difference']">
 
-      <div v-for="act in [1, 2, 3]" class="gap-05">
-        <p class="px-1 bg-[#eee] text-black border-1">Act {{ act }}</p>
-        <div class="flex gap-05">
+      <div v-for="act in [1, 2, 3]" class="flex flex-col gap-0">
+        <p :class="boxClass">Act {{ act }}</p>
+        <div class="flex gap-0">
           <p
             v-for="({ title }) in scene3DParams.filter(s => s.act == act)"
-            class="w-30 px-1 border-1 whitespace-nowrap cursor-pointer"
-            :class="[meta?.title == title ? 'bg-black text-[#eee] border-1 border-black' : 'bg-[#eee] text-black border-1']"
+            class="w-30 whitespace-nowrap cursor-pointer"
+            :class="boxClass"
             @click="() => scene?.initScene?.(scene3DParams.findIndex(s => s.title == title))"
           >{{ title }}</p>
         </div>
